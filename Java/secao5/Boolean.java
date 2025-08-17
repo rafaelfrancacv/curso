3cc2389661d88179503e3d7083334fc383c84694
Rafael Franca <rafaelfrancac6@gmail.com>
2025-08-17 16:55:38 -0300

    Boolean

---
+++ Java/secao5/Boolean.java
@@ -0,0 +1,19 @@
+package secao5;
+
+public class Boolean {
+
+  public static void main(String[] args) {
+    
+    // O que é boolean?
+    boolean isTrue = true;
+    boolean isFalse = false;
+
+    System.out.println(isTrue);
+    System.out.println(isFalse);
+
+    // boolean só aceita true ou false
+
+  }
+
+}
+
