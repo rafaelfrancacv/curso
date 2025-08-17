322e944698e09a0ccb9b86aa9f523abde886616f
Rafael Franca <rafaelfrancac6@gmail.com>
2025-08-17 17:01:41 -0300

    Operadores de Comparação

---
+++ Java/secao5/OperadoresComp.java
@@ -0,0 +1,22 @@
+package secao5;
+
+public class OperadoresComp {
+
+  public static void main(String[] args) {
+  
+    // (==) se é igual / (!=) se é diferente;
+
+    // System.out.println(10 == 10);
+    
+    int x = 10;
+
+    System.out.println(x == 10);
+
+    System.out.println(x == 9);
+
+    System.out.println(x != 5);
+    
+    System.out.println(x != 10);
+  }
+
+}
