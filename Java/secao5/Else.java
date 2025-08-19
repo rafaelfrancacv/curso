e235870c77a881a3fcd6b1e2e19ce584d6852b96
Rafael Franca <rafaelfrancac6@gmail.com>
2025-08-19 14:34:35 -0300

    Else

---
+++ Java/secao5/Else.java
@@ -0,0 +1,21 @@
+package secao5;
+
+public class Else {
+
+  public static void main(String[] args) {
+    
+    int q = 7;
+
+    if (q > 10) {
+
+      System.out.println("Q é maior que 10");
+      
+    } else{
+      System.out.println("Insira um número maior que 10");
+    }
+
+    // Todo else precisa de um if, mas nem todo if precisa de um else
+
+  }
+  
+}
