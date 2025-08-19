85dfabd7f18e42593866ea002c5e8feac559942d
Rafael Franca <rafaelfrancac6@gmail.com>
2025-08-19 13:46:42 -0300

    If

---
+++ Java/secao5/If.java
@@ -0,0 +1,31 @@
+package secao5;
+
+public class If {
+
+  public static void main(String[] args) {
+    
+    int numero = 10;
+
+    if (numero > 5) {
+
+      System.out.println("o número é maior que 5");
+
+    }
+
+    // If com strings
+    String texto = "Teste";
+
+    // Se a função retorna um booleano, eu posso usar no if
+
+    if (texto.equals("Teste")) {
+
+      System.out.println("O texto é: Teste");
+      
+    }
+
+
+    // Declaração do if (Comparação ou retorno de booleano) { O que será executado... }
+
+  }
+
+}
