e87f5ea89069167133463e1c01ba1c35e94fa07d
Rafael Franca <rafaelfrancac6@gmail.com>
2025-08-20 17:01:31 -0300

    Operadores Lógicos

---
+++ Java/secao5/OperadoresLogicos.java
@@ -0,0 +1,15 @@
+package secao5;
+
+public class OperadoresLogicos {
+
+  public static void main(String[] args) {
+    
+    // && é (and / e) retorna true se ambas condicoes forem verdadeiras
+
+    // ||  é (or / ou) retorna true se pelo menos uma das condicoes for verdadeira
+
+    // ! (NOT / NÃO) inverte o valor logico; retorna true se a condicao for falsa e vice-versa
+
+  }
+
+}
