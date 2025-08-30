e0df4b0fab19bf8ad94af42117aa32b200fe7b47
Rafael Franca <rafaelfrancac6@gmail.com>
2025-08-30 15:34:40 -0300

    Exercício01

---
+++ Java/secao5/Exercicios/Exercicio02.java
@@ -0,0 +1,31 @@
+package secao5.Exercicios;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("Digite o nome do seu usuário");
+    String user = sc.nextLine();
+
+    System.out.println("Digite sua senha de 4 dígitos");
+    int senha = sc.nextInt();
+
+    if (user.equals("admin") && senha == 1234) {
+
+      System.out.println("Acesso permitido");
+
+    } else{
+
+      System.out.println("Acesso negado");
+
+    }
+
+    sc.close();
+
+  }
+  
+}
