59b79b43726e0aadc6193e524da5431f6ccfbdb1
Rafael Franca <rafaelfrancac6@gmail.com>
2025-09-08 17:24:08 -0300

    Exercicio03 - completo

--- Java/secao5/Exercicios/Exercicio03.java
+++ Java/secao5/Exercicios/Exercicio03.java
@@ -4,7 +4,21 @@ import java.util.Scanner;
 
 public class Exercicio03 {
 
-  Scanner sc = new Scanner(System.in);
+  public static void main(String[] args) {
+    
+    Scanner sc = new Scanner(System.in);
 
+    System.out.println("Digite um número: ");
+    int numero = sc.nextInt();
+
+    if (numero % 2 == 0) {
+      System.out.println("Seu Número: " + numero + " é PAR!");
+    } else{
+      System.out.println("Seu Número: " + numero + " é IMPAR!");
+    }
+
+    sc.close();
+
+  }
   
 }
