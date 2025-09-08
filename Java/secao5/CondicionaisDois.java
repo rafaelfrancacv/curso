070040dea4ad472555107f064b5fddceae5348ab
Rafael Franca <rafaelfrancac6@gmail.com>
2025-09-08 20:17:53 -0300

    CondicionaisDois

---
+++ Java/secao5/CondicionaisDois.java
@@ -0,0 +1,19 @@
+package secao5;
+
+public class CondicionaisDois {
+
+  public static void main(String[] args) {
+    
+    int numero = 10;
+
+    String resultado = (numero % 2 == 0) ? "Par" : "Impar";
+
+    System.out.println(resultado);
+
+
+    // CONDICAO X > 5 ? EXPRESSAO SE É TRUE "OK" : SE É FALSO "NAO OK";
+
+
+  }
+
+}
