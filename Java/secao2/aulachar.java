26f332e92b4cfb80c68bdf49dc81ff2d6d4e393d
Rafael Franca <rafaelfrancac6@gmail.com>
2025-09-08 16:52:30 -0300

    Correção Char

--- Java/secao2/aulachar.java
+++ Java/secao2/aulachar.java
@@ -4,13 +4,13 @@ public class aulachar {
   
   public static void main(String[] args) {
     
-    //as aspas do char podem ser apenas simples, ex: '', e não "duplas";
+    // As aspas do char podem ser apenas simples, ex: '', e não "duplas";
 
     char letra = 'A';
 
     System.out.println(letra);
 
-    //é uma linguagem que aceita apenas um caractere;
+    // É uma linguagem que aceita apenas um caractere;
 
   }
 
