29f666806f0cb2640159910ee9463087476030b3
Rafael Franca <rafaelfrancac6@gmail.com>
2025-09-09 14:23:32 -0300

    Parametros

---
+++ Java/secao8/Parametros.java
@@ -0,0 +1,20 @@
+package secao8;
+
+public class Parametros {
+
+  public static void main(String[] args) {
+    
+    soma (2, 4);
+    soma (12, 42);
+
+  }
+
+  public static void soma (int a, int b){
+
+    int resultado = a + b;
+
+    System.out.println("O resultado da soma é: " + resultado);
+
+  }
+  
+}
