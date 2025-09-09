2163b254f6ef6cb7b0ce62cf0316ed817a66422f
Rafael Franca <rafaelfrancac6@gmail.com>
2025-09-09 14:17:53 -0300

    Funções

---
+++ Java/secao8/Funcoes.java
@@ -0,0 +1,20 @@
+package secao8;
+
+public class Funcoes {
+
+  public static void main(String[] args) {
+    
+    // 1 - criando funções
+    saudacao();
+
+  }
+
+  // Nivel de acesso, statica => nao preciso instanciar classe para executar (ignorar por enquanto), void: tipo de retorno
+  // Nome, parenteses (ARGS), Bloco {}
+  public static void saudacao(){
+
+    System.out.println("Olá, esta é minha primeira função!");
+
+  }
+  
+}
