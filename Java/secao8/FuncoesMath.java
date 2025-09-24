35ecd712326268aa52805bddaf9069ad24ff7829
Rafael Franca <rafaelfrancac6@gmail.com>
2025-09-24 12:45:27 -0300

    FuncoesMath

---
+++ Java/secao8/FuncoesMath.java
@@ -0,0 +1,18 @@
+package secao8;
+
+public class FuncoesMath {
+
+  public static void main(String[] args) {
+    
+    // Raiz quadrada
+    System.out.println(Math.sqrt(26));
+    System.out.println(Math.sqrt(9));
+
+    //potencia
+    System.out.println(Math.pow(2,3));
+
+
+  }
+
+  
+}
